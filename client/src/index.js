f75a6d578a5a8e5e0d59855f942c77065b21e32e
benjaminadk <benjaminadk@gmail.com>
2018-06-18 04:08:38 -0700

    fix service wrk

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import registerServiceWorker from './registerServiceWorker'
 import { BrowserRouter } from 'react-router-dom'
 import { MuiThemeProvider } from '@material-ui/core/styles'
 import theme from './styles/theme'
