a23f7e0cac8df893e1f403dadb05cd323a8190f4
benjaminadk <benjaminadk@gmail.com>
2018-06-30 01:55:02 -0700

    remove testing

--- client/src/__tests__/Home.test.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { shallow } from 'enzyme'
-import Home from '../containers/Home'
-
-it('expect to render Home component', () => {
-  expect(shallow(<Home />)).toMatchSnapshot()
-})
--- client/src/__tests__/Root.BottomNav.test.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { shallow } from 'enzyme'
-import BottomNav from '../components/Root.BottomNav'
-
-it('expect to render Home component', () => {
-  expect(shallow(<BottomNav />)).toMatchSnapshot()
-})
