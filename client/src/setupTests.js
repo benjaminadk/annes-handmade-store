c58abcb86234144fb56cca0e7eb5391bb994b3b9
benjaminadk <benjaminadk@gmail.com>
2018-07-05 09:57:14 -0700

    latest stripe attempt

--- client/src/setupTests.js
+++
@@ -1,7 +0,0 @@
-import Enzyme from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
-import 'jest-enzyme'
-import 'react-testing-library/cleanup-after-each'
-import 'jest-dom/extend-expect'
-
-Enzyme.configure({ adapter: new Adapter() })
